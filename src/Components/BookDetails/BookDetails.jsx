502d0d91c19403eda20e599088a8f06dd1f3692e
mohammad syed hasan <syedhasanmohammad@gmail.com>
2024-03-27 01:38:52 +0600

    completed Book Details

--- src/Components/BookDetails/BookDetails.jsx
+++ src/Components/BookDetails/BookDetails.jsx
@@ -4,11 +4,107 @@ const BookDetails = () => {
   const { userId } = useParams();
   const books = useLoaderData();
   const book = books.find((book) => book.bookId === parseInt(userId));
-  console.log(book);
 
   return (
     <div>
-      <h2>Details oF id : </h2>
+      <section className="dark:bg-gray-100 dark:text-gray-800">
+        <div className="container flex flex-col items-center justify-center py-6 mx-auto  lg:flex-row lg:justify-center gap-12">
+          {/* Image */}
+          <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 lg:h-[740px] sm:h-80  xl:h-112 2xl:h-128 bg-[#1313130D] lg:w-[575px] rounded-2xl">
+            <img
+              src={book.image}
+              alt=""
+              className="w-[425px] rounded-2xl h-[565px]"
+            />
+          </div>
+
+          {/* text */}
+          <div className="flex flex-col   text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+            <h1 className="text-3xl md:text-[40px] font-bold">
+              {book.bookName}
+            </h1>
+
+            {/* author name */}
+            <p className="mt-5 pb-5 border-b-[1px] border-dashed border-[#13131326] text-[#131313] text-opacity-60 font-semibold work-sans-font text-lg">
+              By : {book.author}
+            </p>
+
+            {/* category */}
+            <div className="py-5 border-b-[1px] border-dashed border-[#13131326] text-[#131313] text-opacity-60 font-semibold work-sans-font text-lg mb-6">
+              {book.category}
+            </div>
+
+            {/* Review */}
+
+            <div className="mb-12">
+              <span className="font-bold work-sans-font text-base">
+                Review :
+              </span>
+              <p className="inline text-[#131313B3] font-medium work-sans-font text-base leading-[26px] ">
+                {book.review}
+              </p>
+            </div>
+
+            {/* Tag */}
+
+            <div className="flex  gap-7 pb-7 border-b-[1px] border-dashed border-[#13131326]">
+              <h3 className="font-bold work-sans-font text-base self-center">
+                Tag
+              </h3>
+              <ul className=" flex items-center gap-3 ">
+                {book.tags.map((tag, idx) => (
+                  <li
+                    className="px-4 py-2 rounded-full text-[#23BE0A] font-medium text-sm  whitespace-nowrap  bg-[#F3F3F3]"
+                    key={idx}
+                  >
+                    #{tag}
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* Details */}
+            <div className="space-y-3 mt-6">
+              <div className="grid grid-cols-2 gap-16">
+                <h2 className="text-[#131313] text-opacity-60 font-semibold work-sans-font">
+                  Number of Pages:
+                </h2>
+                <p className="font-bold">{book.totalPages}</p>
+              </div>
+              {/* 2 */}
+              <div className="grid grid-cols-2 gap-16">
+                <h2 className="text-[#131313] text-opacity-60 font-semibold work-sans-font">
+                  Publisher:
+                </h2>
+                <p className="font-bold">{book.publisher}</p>
+              </div>
+              {/* 3*/}
+              <div className="grid grid-cols-2 gap-16">
+                <h2 className="text-[#131313] text-opacity-60 font-semibold work-sans-font">
+                  Year of Publishing:
+                </h2>
+                <p className="font-bold">{book.yearOfPublishing}</p>
+              </div>
+              {/* 4*/}
+              <div className="grid grid-cols-2 gap-16">
+                <h2 className="text-[#131313] text-opacity-60 font-semibold work-sans-font">
+                  Rating:
+                </h2>
+                <p className="font-bold">{book.rating}</p>
+              </div>
+            </div>
+            {/* buttons */}
+            <div className="flex flex-col space-y-4 mt-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start text-lg font-bold">
+              <a className="px-8 py-3 text-black border-[1px] solid border-[#1313134D] text-lg font-bold   h-auto        btn rounded-md">
+                Read
+              </a>
+              <a className="px-8 py-3 text-lg font-bold h-auto btn bg-[#50B1C9] text-white rounded-md">
+                Wishlist
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
